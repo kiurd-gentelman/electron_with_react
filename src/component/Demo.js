a27e5e823d508ad99bd39b0e8f8f70f5d320863a
Imtiaz Ur Rahman Khan <65729806+kiurd-gentelman@users.noreply.github.com>
2021-11-24 01:32:10 +0600

    project upload

---
+++ src/component/Demo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                About
+            </div>
+        );
+    }
+}
+
+export default Demo;
